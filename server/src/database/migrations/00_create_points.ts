30812e9b47871483ed2532f1a3c31ce31307c52b
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-08-23 10:16:23 -0300

    Update

--- server/src/database/migrations/00_create_points.ts
+++ server/src/database/migrations/00_create_points.ts
@@ -1,11 +1,26 @@
+import Knex from 'knex';
 
 /* - */
-export async function up() {
-    
+export async function up(knex: Knex) {
+    //Criar a tabela
+    knex.schema.createTable('points', table => {
+        table.increments('id').primary();
+        table.string('image').notNullable();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('whatsapp').notNullable();
+        table.decimal('latitude').notNullable();
+        table.decimal('longitude').notNullable();
+        table.string('city').notNullable();
+        table.string('uf').notNullable();
+    });
+
 }
 
 
 /* - */
 export async function down() {
-    
+    //Voltar atras (Deletar a tabela)
+
+
 }
